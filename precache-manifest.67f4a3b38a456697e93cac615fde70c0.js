a4960cd45565543a53dda7dd0f3131b1c8db171b
Angelica Miranda <mirandas.angelica@gmail.com>
2020-06-24 17:11:16 -0500

    deploy

---
+++ precache-manifest.67f4a3b38a456697e93cac615fde70c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be659576af0593cb8ece",
+    "url": "/css/app.6a38a644.css"
+  },
+  {
+    "revision": "49f83a39f01fa66a2c2e",
+    "url": "/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "82079f23cc8f6e5f0ef19c33c055baf6",
+    "url": "/img/circle.png"
+  },
+  {
+    "revision": "ecc4cf252a967f043b7cc229560486ac",
+    "url": "/img/github.png"
+  },
+  {
+    "revision": "11d38b71ab182e64a9440177f00030d4",
+    "url": "/img/gitlab.png"
+  },
+  {
+    "revision": "185b34606a228d2843220b2612aaac28",
+    "url": "/img/twitter.png"
+  },
+  {
+    "revision": "6b668e4ef096badb83a396288b416c09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be659576af0593cb8ece",
+    "url": "/js/app.a5716201.js"
+  },
+  {
+    "revision": "49f83a39f01fa66a2c2e",
+    "url": "/js/chunk-vendors.ead441a4.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
